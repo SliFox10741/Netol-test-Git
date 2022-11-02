dddd197c1b8ed821e058a1da5b7f57c4e79b5daa
Cookie107 <mr.maximgus@gmail.com>
2022-11-02 23:33:22 +0500

    Замена на long

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] price = {13, 12, 10, 2, 31, 4};
+        long[] price = {13, 12, 10, 2, 31, 4};
         SalesManager sale = new SalesManager(price);
-        int max = sale.max();
+        long max = sale.max();
         System.out.println("Максимальное значение - " + max);
     }
 }
